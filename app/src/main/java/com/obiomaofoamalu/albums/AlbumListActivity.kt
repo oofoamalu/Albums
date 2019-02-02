ca8efe38b35cb67a3a5bda84ae5e661dd778bd74
andela-oofoamalu <jessicaprincess61@gmail.com>
2019-02-02 20:50:59 +0100

    Project Initialization.

---
+++ app/src/main/java/com/obiomaofoamalu/albums/AlbumListActivity.kt
@@ -0,0 +1,12 @@
+package com.obiomaofoamalu.albums
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AlbumListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_album_list)
+    }
+}
